ac3339a1fc10fd4564ca4455552837b2148afe49
Eaton Wu <ew9170@rit.edu>
2024-06-07 00:44:59 -0700

    Added website discovery crate

--- company_scraper/src/main.rs
+++ company_scraper/src/main.rs
@@ -1,13 +1,20 @@
 use company_scraper;
+use company_scraper::get_company_idx_file_from_sec;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>>{
-    // get_company_idx_file_from_sec().await?;
+    get_company_idx_file_from_sec().await?;
     let companies = company_scraper::get_companies_from_idx()?;
 
     let mut data_store = company_scraper::process_raw_data(companies);
-    let filtered_companies = company_scraper::filter_data(&data_store,
-                                                          vec!["inc", "corp", "llc", "ltd", "group"]);
+    let filter = vec! [
+        "inc",
+        "corp",
+        "llc",
+        "ltd",
+        "group"
+    ];
+    let filtered_companies = company_scraper::filter_data(&data_store, filter);
     data_store.replace_data(&filtered_companies);
     for company in &filtered_companies {
         println!("{:?}", company);
