b6f18a299bb88389d0a174e95d2f5a15a3d4a11a
Eaton Wu <ew9170@rit.edu>
2024-07-19 19:10:28 -0700

    main stuff

---
+++ serp_service/src/main.rs
@@ -0,0 +1,19 @@
+use serp_service::{GoogleSerpService, SerpService};
+
+#[tokio::main]
+async fn main() {
+    // print working directory
+    let cwd = std::env::current_dir().unwrap();
+    println!("Current working directory: {:?}", cwd);
+    let serp_service = GoogleSerpService::new(None);
+    match serp_service.get_serp("test") {
+        Ok(v) => {
+            for (title, url) in v {
+                println!("Title: {}, URL: {}", title, url);
+            }
+        },
+        Err(e) => {
+            println!("Error: {:?}", e);
+        }
+    }
+}
